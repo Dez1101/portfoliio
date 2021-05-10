cbe2da7d6fb119a40318abf13d37ca954876d8ea
Dez <dkay1101@gmail.com>
2021-05-10 13:43:03 +0200

    added media url and root

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# models.Model allows us us to create this new class in python called Jobs snd gives us permission to save 
+# this object or to be clear the attributes of these objects into the database
+
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+    #the 2 variables above image and summary are the attributes/properties to be save to the database
+    #to know where to get the fields for the models google search django models field
