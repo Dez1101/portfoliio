43e5f71022304cc08f2689ce520fc707443a9f21
Desire Kabwanga <dkay1101@gmail.com>
2021-05-26 14:48:32 +0200

    completed the blog page

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,19 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     blog_title = models.CharField(max_length=60)
-    pub_date = models.DateField()
+    pub_date = models.DateTimeField()
     body_text = models.TextField()
     blog_img = models.ImageField(upload_to='images/')
     id = models.AutoField(primary_key=True)
 
+    # display the titles of the blogs when in admin page
+    def __str__(self):
+        return self.blog_title
+
+    # return the first 100 words of the blog texts
     def summary(self):
         return self.body_text[:100]
+
+    # return only the date and not time and display in the format month day year
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
-def allblogs(request):
+def blogs(request):
     blogs = Blog.objects
     return render(request, 'blog/blogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    # return render(request,)
+    blogdetail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html',{'blog': blogdetail})
